d460313b50be1bc3d72798e0786d819f11562b1e
Pierre Bhoorasingh <bhoorasingh.p@husky.neu.edu>
2016-03-03 22:09:23 -0500

    Render user input and redirect to graph page

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, request, redirect
+import requests
 
 app = Flask(__name__)
 
+app.vars={}
 @app.route('/')
 def main():
     return redirect('/index')
 
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def index():
-  return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        for key in request.form.keys():
+            app.vars[key] = request.form[key]
+        return redirect('/graph')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')#port=33507)
